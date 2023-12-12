6a84d55297d934f0ca93707170cee16a50f770e1
askarbek0va <askarbekovameerim08@gmail.com>
2023-12-13 00:34:38 +0600

    fifth commit

--- modified_sea_battle.py
+++ modified_sea_battle.py
@@ -1,6 +1,19 @@
+import os
 import random
 name=input('Please enter your name:')
-n = int(input()) 
-a = []
-for i in range(n):
-    a.append([int(j) for j in input().split()])
+print('Now we are starting to play')
+print("A B C D E F G")
+a = [['o'] * 7 for i in range(7)] 
+for row in a:
+    print(' '.join(row))
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def ships_location():
+    ships = []
+    for ship_size in [3, 2, 2, 1, 1, 1, 1]:
+        ship_coordinates = ships_location(ship_size, ships)
+        ships.extend(ship_coordinates)
+    return ships
+
